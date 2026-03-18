272175d3ba2e893cfa75ca7efee6ca572b08b157
Jakub Bilski <jakub@blsk.pl>
2026-03-18 11:32:14 +0100

    Refactored isEven/isOdd.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 function isEven(value) {
-    if (value % 2 === 0) {
-        return true;
-    }
-
-    return false;
-
-    return (value % 2 === 0 ? true : false);
-
-    retrun (value % 2 === 0);
-
     return !(value % 2);
 }
 

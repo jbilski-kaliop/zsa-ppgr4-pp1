06fc6cd7f28b9e82c81922c68cdab62369edf92a
Jakub Bilski <jakub@blsk.pl>
2026-03-18 12:00:13 +0100

    Generic max implementation.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
 function isEven(value) {
-    return !(value % 2);
+    if (Number.isInteger(value)) {
+        return !(value % 2);
+    }
+
+    return false;
 }
 
 function isOdd(value) {
-    return !isEven(value);
+    if (Number.isInteger(value)) {
+        return !isEven(value);
+    }
+
+    return false;
 }
 
-function max(val1, val2, val3) {
-    if (val1 > val2 && val1 > val3) {
-        return val1;
-    } else if (val2 > val3) {
-        return val2;
-    } else {
-        return val3;
+function max(...values) {
+    if (values.length === 0) {
+        return NaN;
     }
+
+    let i = 0;
+    let result = NaN;
+    for (; i < values.length; i++) {
+        if (!isNaN(values[i])) {
+            result = values[i];
+            break;
+        }
+    }
+
+    for (i = i + 1; i < values.length; i++) {
+        if (!isNaN(values[i]) && values[i] > result) {
+            result = values[i];
+        }
+    }
+
+    return isNaN(result) ? NaN : result;
 }
